84fb095834e3c9a21d19930f39708aeeeb524bfa
antoinequellier <antoinecquellier@gmail.com>
2018-11-04 16:40:20 +0000

    Getting list of 22 followers for a given user

--- index.js
+++ index.js
@@ -13,9 +13,11 @@ const searchUser = `https://www.instagram.com/${userToSearch}`;
 // const searchFollowers = `https://www.instagram.com/${userToSearch}/followers`;
 const followers = `a[href='/${userToSearch}/followers/']`;
 
+const firstFollower = 'a[class="FPmhX notranslate _0imsa "]';
 
 
 (async() => {
+  // headless false for visual debugging in browser
   const browser = await puppeteer.launch({
     headless: false
   });
@@ -29,11 +31,26 @@ const followers = `a[href='/${userToSearch}/followers/']`;
   await page.click(passwordInput);
   await page.keyboard.type(CREDS.password);
   await page.click(submitButton);
-  await page.waitFor(3000)
+  await page.waitFor(3000);
   // Search User with URL
   await page.goto(searchUser);
   await page.click(followers);
-  await page.waitFor(3000)
+  await page.waitFor(3000);
   await page.screenshot({ path: 'screenshots/insta.png' });
+  const followersLength = await page.$$('.FPmhX notranslate _0imsa ');
+  console.log(followersLength);
+
+  // Pb: each time you launch the programm, the list changes and you get a different
+  let firstFollowers = await page.evaluate(() => {
+    let data = [];
+    let elements = document.getElementsByClassName('FPmhX notranslate _0imsa ');
+    for (var element of elements)
+        data.push(element.textContent);
+    return data;
+  });
+  console.log(firstFollowers);
+
+  // Go to first follower profile
+  await page.click(firstFollower);
   // await browser.close();
 })();
