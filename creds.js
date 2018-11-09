09b5b310ac8c86ae8f9c7719001e5249a36fa558
antoinequellier <antoinecquellier@gmail.com>
2018-11-09 10:49:29 +0000

    added watson developer tool

--- creds.js
+++ creds.js
@@ -1,4 +1,5 @@
 module.exports = {
   username: 'robertocaras91',
-  password: 'pdqK8R653xMBMPA'
+  password: 'pdqK8R653xMBMPA',
+  apikey:   '05ddXf06q1Ra78pEwNdO2bScxE_PPEplNQOIln2KI8N0'
 }
--- screenshotDOMElement.js
+++ screenshotDOMElement.js
@@ -1,18 +1,18 @@
-// async function screenshotDOMElement(page, selector, padding = 0) {
-//   console.log(document)
-//   const rect = await page.evaluate(selector => {
-//     const element = document.querySelector(selector);
-//     const {x, y, width, height} = element.getBoundingClientRect();
-//     return {left: x, top: y, width, height, id: element.id};
-//   }, selector);
+async function screenshotDOMElement(page, selector, padding = 0) {
+  console.log(document)
+  const rect = await page.evaluate(selector => {
+    const element = document.querySelector(selector);
+    const {x, y, width, height} = element.getBoundingClientRect();
+    return {left: x, top: y, width, height, id: element.id};
+  }, selector);
 
-//   return await page.screenshot({
-//     path: 'screenshots/element.png',
-//     clip: {
-//       x: rect.left - padding,
-//       y: rect.top - padding,
-//       width: rect.width + padding * 2,
-//       height: rect.height + padding * 2
-//     }
-//   });
-// }
+  return await page.screenshot({
+    path: 'screenshots/element.png',
+    clip: {
+      x: rect.left - padding,
+      y: rect.top - padding,
+      width: rect.width + padding * 2,
+      height: rect.height + padding * 2
+    }
+  });
+}
