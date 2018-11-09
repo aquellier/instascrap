d9d4eb40063f8c192baa7e7816e562b6cb0bf34d
antoinequellier <antoinecquellier@gmail.com>
2018-11-09 11:51:00 +0000

    require creds file in facial recognition

--- facial_recognition.js
+++ facial_recognition.js
@@ -1,16 +1,14 @@
-'use strict';
-
 const VisualRecognitionV3 = require('watson-developer-cloud/visual-recognition/v3');
+const CREDS = require('./creds');
 const fs = require('fs');
 
 const visualRecognition = new VisualRecognitionV3({
-  iam_apikey: '05ddXf06q1Ra78pEwNdO2bScxE_PPEplNQOIln2KI8N0',
+  iam_apikey: CREDS.apikey,
   version: '2018-06-11'
 });
 
 const params = {
   // An image file (.jpg, .png) or .zip file with images
-  // images_file: fs.createReadStream('./screenshots/car.png')
   images_file: fs.createReadStream('screenshots/man_35.png')
 };
 
