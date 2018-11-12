0cd62427aea7bbfa29da3f27c3256a88f6705ef1
antoinequellier <antoinecquellier@gmail.com>
2018-11-12 12:24:47 +0000

    get an array of infos about users based on their picture with watson API

--- facial_recognition.js
+++ facial_recognition.js
@@ -1,38 +1,49 @@
 const VisualRecognitionV3 = require('watson-developer-cloud/visual-recognition/v3');
 const CREDS = require('./creds');
 const fs = require('fs');
+const usersPictures = ['screenshots/man_35.jpg', 'screenshots/man_50.jpg', 'screenshots/woman_30.jpg', 'screenshots/woman_65.jpg'];
 
-const visualRecognition = new VisualRecognitionV3({
-  iam_apikey: CREDS.apikey,
-  version: '2018-06-11'
-});
 
-const params = { images_file: fs.createReadStream('screenshots/man_35.png') };
+let users = [];
+function addUserInfos(picture) {
+  const visualRecognition = new VisualRecognitionV3({
+    iam_apikey: CREDS.apikey,
+    version: '2018-06-11'
+  });
 
-function user(ageMin, ageMax, ageProbability, gender, genderProbability){
-  this.ageMin = ageMin;
-  this.ageMax = ageMax;
-  this.ageProbability = ageProbability;
-  this.gender = gender;
-  this.genderProbability = genderProbability;
+  const params = { images_file: fs.createReadStream(picture) };
+
+  function user(id, ageMin, ageMax, ageProbability, gender, genderProbability){
+    this.id = id;
+    this.ageMin = ageMin;
+    this.ageMax = ageMax;
+    this.ageProbability = ageProbability;
+    this.gender = gender;
+    this.genderProbability = genderProbability;
+  }
+
+  visualRecognition.detectFaces(params, function(err, res) {
+    if (err) {
+      console.log("There is an error");
+    } else {
+      const id = users.length;
+      const faceInfos = res.images[0].faces[0];
+      const ageMin = faceInfos.age.min;
+      const ageMax = faceInfos.age.max;
+      const ageProbability = faceInfos.age.score;
+      const gender = faceInfos.gender.gender;
+      const genderProbability = faceInfos.gender.score;
+      const userInfos = new user(id, ageMin, ageMax, ageProbability, gender, genderProbability);
+      // console.log(userInfos);
+      users.push(userInfos);
+    }
+  });
 }
 
-
-visualRecognition.detectFaces(params, function(err, res) {
-  if (err) {
-    console.log("There is an error");
-  } else {
-    const faceInfos = res.images[0].faces[0]
-    const ageMin = faceInfos.age.min
-    const ageMax = faceInfos.age.max
-    const ageProbability = faceInfos.age.score
-    const gender = faceInfos.gender.gender
-    const genderProbability = faceInfos.gender.score
-    var userInfos = new user(ageMin, ageMax, ageProbability, gender, genderProbability);
-    console.log(userInfos);
-    return userInfos;
-  }
+usersPictures.forEach((picture) => {
+  addUserInfos(picture);
 });
 
-
-// console.log(userInfos);
+setTimeout(() => {
+  console.log(users);
+}, 3000);
