06b5f7da098757ba3e2534a7d99a2293e15da70d
antoinequellier <antoinecquellier@gmail.com>
2018-11-12 15:21:38 +0000

    webpack config, fails to compile because of deps of puppeteer

--- webpack.config.js
+++ webpack.config.js
@@ -6,5 +6,12 @@ module.exports = {
     publicPath: "dist",
     filename: "bundle.js"
   },
-  devtool: "sourcemap"
+  devtool: "sourcemap",
+  node: {
+    fs: 'empty'
+  },
 };
+
+
+// this is in the “plugins” array part of our webpack object
+    // next 3 lines added to remove warnings....might be a problem
